be64c0f1db8c277849254151a94db7245b1a3292
Andreas Kugel <andreas.kugel@web.de>
2025-10-18 19:27:00 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<canvas id="renderCanvasContainer" style="width: 800px; height: 600px; border: 1px solid black;"></canvas>
+<div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+import buildCanvas from './bab2';
+
+buildCanvas(document.getElementById('renderCanvasContainer')!); 
+
